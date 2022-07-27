e24e6df1de8e8380a020a9e52b4724734e09c889
Gustavo <gustavogmfarias@gmail.com>
2022-07-26 22:28:48 -0300

    primeira tentativa de teste

--- src/modules/accounts/useCases/createUser/tests/CreateCategoryController.spec.ts
+++ src/modules/accounts/useCases/createUser/tests/CreateCategoryController.spec.ts
@@ -1,65 +1,62 @@
-import request from "supertest";
+/**
+ * @jest-environment ./prisma/prisma-environment-jest
+ */
+
 import { app } from "@shared/infra/http/app";
+import request from "supertest";
 
-import createConnection from "@shared/infra/typeorm";
-import { Connection } from "typeorm";
-import { hash } from "bcryptjs";
-import { v4 as uuidV4 } from "uuid";
-
-let connection: Connection;
-
-describe("Create Category Controller", () => {
-    beforeAll(async () => {
-        connection = await createConnection();
-
-        await connection.runMigrations();
-
-        const password = await hash("admin", 8);
-        const id = uuidV4();
-        await connection.query(`
-    INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
-    values('${id}', 'admin', 'admin@admin.com', '${password}', true, 'now()', '564654' )
-    `);
-    });
-
-    afterAll(async () => {
-        await connection.dropDatabase();
-        await connection.close();
-    });
-
-    it("should be able to create a new category", async () => {
+describe("Create User Controller", () => {
+    it("Should be able to create a new user", async () => {
         const responseToken = await request(app)
             .post("/sessions")
             .send({ email: "admin@admin.com", password: "admin" });
 
-        const { refresh_token } = responseToken.body;
+        const { token } = responseToken.body;
 
         const response = await request(app)
-            .post("/categories")
+            .post("/users")
+            .set({ Authorization: `Bearer ${token}` })
             .send({
-                name: "22",
-                description: "Categoria de carro suv",
-            })
-            .set({ Authorization: `Bearer ${refresh_token}` });
+                email: "testIntegration@test.com.br",
+                name: "Test ",
+                lastName: "Integration",
+                password: "test",
+            });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("id");
 
-        expect(response.status).toBe(201);
+        console.log("teste");
     });
 
-    it("should not be able to create a new category with the same name than other", async () => {
+    it("Should not be able to create an existing user", async () => {
         const responseToken = await request(app)
             .post("/sessions")
             .send({ email: "admin@admin.com", password: "admin" });
 
-        const { refresh_token } = responseToken.body;
+        const { token } = responseToken.body;
 
-        const response = await request(app)
-            .post("/categories")
+        await request(app)
+            .post("/users")
+            .set({ Authorization: `Bearer ${token}` })
             .send({
-                name: "22",
-                description: "Categoria de carro suv",
-            })
-            .set({ Authorization: `Bearer ${refresh_token}` });
+                email: "testIntegration@test.com.br",
+                name: "Test ",
+                lastName: "Integration",
+                password: "test",
+            });
+
+        const response = await await request(app)
+            .post("/users")
+            .set({ Authorization: `Bearer ${token}` })
+            .send({
+                email: "testIntegration@test.com.br",
+                name: "Test ",
+                lastName: "Integration",
+                password: "test",
+            });
 
         expect(response.status).toBe(400);
+
+        console.log("teste2");
     });
 });
