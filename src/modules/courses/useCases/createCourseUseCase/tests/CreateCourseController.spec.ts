18ff2c5815d394f441a10418a40c372833b64379
gustavogmfarias <gustavogmfarias@gmail.com>
2023-01-18 09:58:02 -0300

    .

--- src/modules/courses/useCases/createCourseUseCase/tests/CreateCourseController.spec.ts
+++ src/modules/courses/useCases/createCourseUseCase/tests/CreateCourseController.spec.ts
@@ -3,10 +3,11 @@
  */
 
 import request from "supertest";
+import { SchoolLevel } from "@prisma/client";
 import { AppError } from "../../../../../shared/errors/AppError";
 import { app } from "../../../../../shared/infra/http/app";
 
-describe("Create TextualGenre Controller", () => {
+describe("COURSES - Create Course Controller", () => {
     let token: string;
 
     beforeAll(async () => {
@@ -17,48 +18,52 @@ describe("Create TextualGenre Controller", () => {
         token = loginAdmin.body.token;
     });
 
-    it("Should be able to create a new textualGenre", async () => {
+    it("Should be able to create a new course", async () => {
         const response = await request(app)
-            .post("/textualgenre")
+            .post("/courses")
             .set({ Authorization: `Bearer ${token}` })
             .send({
-                name: "Teste De Genero 1",
+                name: "Teste De Course 1",
+                schoolLevel: "SUPERIOR",
             });
 
-        expect(response.body.textualGenre.name).toBe("Teste De Genero 1");
+        expect(response.body.course.name).toBe("Teste De Course 1");
         expect(response.body.log.description).toBe(
-            "Textual Genre created successfully!"
+            "Course created successfully!"
         );
 
         expect(response.status).toBe(201);
     });
 
-    it("Should not be able to create a new textualGenre with same name", async () => {
+    it("Should not be able to create a new course with same name", async () => {
         const response = await request(app)
-            .post("/textualgenre")
+            .post("/courses")
             .set({ Authorization: `Bearer ${token}` })
             .send({
-                name: "Teste De Genero 1",
+                name: "Teste De Course 1",
+                schoolLevel: "SUPERIOR",
             });
 
-        expect(response.body.message).toBe("Textual Genre already exists");
+        expect(response.body.message).toBe("Course already exists");
         expect(response.status).toBe(400);
     });
 
-    it("Should not be able to create a new textualGenre if the token is invalid", async () => {
+    it("Should not be able to create a new course if the token is invalid", async () => {
         const response = await request(app)
-            .post("/textualgenre")
+            .post("/courses")
             .set({ Authorization: `Bearer 111111` })
             .send({
                 name: "test2",
+                schoolLevel: "SUPERIOR",
             });
 
         expect(response.body.message).toBe("Invalid Token");
     });
 
-    it("Should not be able to create a new Categort if user is not logged", async () => {
-        const response = await request(app).post("/textualgenre").send({
+    it("Should not be able to create a new Course if user is not logged", async () => {
+        const response = await request(app).post("/courses").send({
             name: "test2",
+            schoolLevel: "SUPERIOR",
         });
 
         expect(response.body.message).toBe("Token missing");
